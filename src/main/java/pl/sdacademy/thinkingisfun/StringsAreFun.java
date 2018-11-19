4427fce7ba501f2925fce06efd72d4f99afe9cb2
arqarq <43341071+arqarq@users.noreply.github.com>
2018-11-19 19:44:26 +0100

    - StringsAreFun.java 1/3

--- src/main/java/pl/sdacademy/thinkingisfun/StringsAreFun.java
+++ src/main/java/pl/sdacademy/thinkingisfun/StringsAreFun.java
@@ -1,7 +1,6 @@
 package pl.sdacademy.thinkingisfun;
 
 public class StringsAreFun {
-
     public boolean isPalindrome(String toBeTested) {
         return false;
     }
@@ -11,6 +10,18 @@ public class StringsAreFun {
     }
 
     public String rot13(String toBeCoded) {
-        return "";
+        char[] toBeCodedAsArray = toBeCoded.toCharArray();
+        for (int i = 0; i < toBeCodedAsArray.length; i++) {
+            if (toBeCodedAsArray[i] > 96 && toBeCodedAsArray[i] < 110) {
+                toBeCodedAsArray[i] = (char) (toBeCodedAsArray[i] + 13);
+            } else if (toBeCodedAsArray[i] > 109 && toBeCodedAsArray[i] < 123) {
+                toBeCodedAsArray[i] = (char) (toBeCodedAsArray[i] - 13);
+            } else if (toBeCodedAsArray[i] > 64 && toBeCodedAsArray[i] < 78) {
+                toBeCodedAsArray[i] = (char) (toBeCodedAsArray[i] + 13);
+            } else if (toBeCodedAsArray[i] > 77 && toBeCodedAsArray[i] < 91) {
+                toBeCodedAsArray[i] = (char) (toBeCodedAsArray[i] - 13);
+            }
+        }
+        return new String(toBeCodedAsArray);
     }
 }
